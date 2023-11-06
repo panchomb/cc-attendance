7975b98ca805b1609c803c328644bd2536e7cd06
panchomb <francisco.magot@utec.edu.pe>
2023-11-06 12:50:54 +0100

    added websocket functionality and submit and view attendees

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
-#from flask_socketio import SocketIO
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-#socketio = SocketIO(app)
+socketio = SocketIO(app)
 
 code = ''
 
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,19 @@
-from app import app
+from app import app, socketio
 from flask import render_template, request, jsonify
 import random
 import string
+import threading
 import time
 
-code = ''
-timer = 30
+code = None
+next_update_time = None
+timer_thread = None
+attendees = {}
+attendees['attendees'] = []
+stop_timer_thread = threading.Event()
 
 @app.route('/', methods=["GET"])
 def home():
-    '''
-    This function returns the home page.
-    '''
     return render_template('index.html')
 
 @app.route('/code', methods=["GET"])
@@ -26,25 +28,96 @@ def submit_attendance():
 def verify_attendance():
     return render_template('verify_attendance.html')
 
-@app.route('/get_code', methods=["GET"])
-def get_code():
-    '''
-    This function generates a 6 character alphanumeric code.
-    '''
-    global code
-    if code == '':
-        code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-    return code
+@socketio.on('connect', namespace='/code')
+def handle_code_connection():
+    global next_update_time, code, timer_thread
+
+    # If there is no code being generated, start the timer and generate a code
+    if timer_thread == None:
+        code = generate_random_code()
+        next_update_time = time.time() + 30
+        print('generating code from new connection')
+        timer_thread = threading.Thread(target=update_code_timer)
+        timer_thread.start()
+
+    print(f'Client connected {request.remote_addr}')
+    
+    socketio.emit('code_update', {'code': code, 'current_time': time.time(), 'next_update_time': next_update_time}, namespace='/code')
+
+@socketio.on('disconnect', namespace='/code')
+def handle_code_disconnection():
+    global timer_thread
+
+    print(f'Client disconnected {request.remote_addr}')
+
+def update_code_timer():
+    global code, next_update_time
+
+    while not stop_timer_thread.is_set():
+        if time.time() >= next_update_time:
+            code = generate_random_code()
+            next_update_time = time.time() + 30
+            socketio.emit('code_update', {'code': code, 'current_time': time.time(), 'next_update_time': next_update_time}, namespace='/code')
 
 @app.route('/check_attendance', methods=["POST"])
 def check_attendance():
     global code
+
+    response = {}
+
+    if code == None:
+        response['invalid'] = True
+        return jsonify(response)
+
     request_data = request.get_json()
     user_code = request_data.get('code', '')
     
     if user_code == code:
-        response = {'verification': 'success'}
+        response['verification'] = 'success'
+        attendees['attendees'].append(time.time())
+        print(attendees)
     else:
-        response = {'verification': 'failure'}
+        response['verification'] = 'failure'
+
+    response['invalid'] = False
     
     return jsonify(response)
+
+@app.route('/stop_attendance', methods=["POST"])
+def stop_attendance():
+    global timer_thread, code, next_update_time, stop_timer_thread
+    response = {}
+
+    if timer_thread and timer_thread.is_alive():
+        stop_timer_thread.set()
+
+        timer_thread.join(timeout=1)
+
+        if timer_thread.is_alive():
+            print('failed to stop attendance')
+            response['status'] = 'failure'
+        else:
+            response['status'] = 'success'
+            timer_thread = None
+            code = None
+            next_update_time = None
+            attendees['attendees'] = []
+            stop_timer_thread.clear()
+            print('stopping attendance')
+    else:
+        print('error stopping attendance')
+        response['status'] = 'error'
+
+    return jsonify(response)
+
+@app.route('/view_attendees', methods=["GET"])
+def view_attendees():
+    global attendees
+
+    return jsonify(attendees)
+
+def generate_random_code():
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
